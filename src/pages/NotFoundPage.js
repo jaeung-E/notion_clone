cf9139cca1f48c703ec1cf50c99eb2890bf55396
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2023-02-01 15:00:32 +0900

    docs: NotFoundPage 내용 변경

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,17 @@
+import { push } from "../utils/router.js";
+
 export default function NotFoundPage({ $target }) {
   this.render = () => {
     $target.innerHTML = `
-      <h1>존재하지 않는 페이지 입니다!</h1>
+      <div class='not-found-container'>
+        <div>&#x1F625</div>
+        <h1>존재하지 않는 페이지 입니다!</h1>
+        <button class='home-button'>Home</button>
+      </div>
     `;
+
+    document.querySelector(".home-button").addEventListener("click", () => {
+      push("/");
+    });
   };
 }
